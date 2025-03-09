35e286bd92c61e5f0d4736b331de965157c6de40
aslaha@microsoft.com <ashislaha@ASHISs-MacBook-Pro.local>
2025-03-09 16:40:02 -0700

    try-except

--- try_block.py
+++ try_block.py
@@ -6,9 +6,6 @@ def sum(num1, num2):
     else:
         print("No error found")
 
-input1 = input("Enter a number")
-sum(input1, 10)
-
 def file_operation():
     try:
         file = open("ab.txt", "r") # file does not exist, so reading it will throw an error
@@ -18,5 +15,25 @@ def file_operation():
         print("There is an OS error")
     except:
         print("File open error")
+    finally:
+        print("File handling is done")
 
-file_operation()
+
+def scan_a_number():
+    while True:
+        try:
+            number = int(input("Enter a number: "))
+        except:
+            print("Please enter a number!!!")
+            continue
+        else:
+            print(number)
+            break # user enters a valid number
+        finally:
+            print("scanning is completed")
+
+if __name__ == "__main__":
+    input1 = input("Enter a number")
+    sum(input1, 10)
+    file_operation()
+    scan_a_number()
