79ae56d6e3bcd1fe3493288a1d8fd453e651eff1
aslaha@microsoft.com <ashislaha@ASHISs-MacBook-Pro.local>
2025-04-05 20:00:58 -0700

    added debugger mechanism

--- math_explore/math_explore.py
+++ math_explore/math_explore.py
@@ -1,4 +1,5 @@
 import math
+import pdb
 import random
 
 def generate_random_between_range(a,b):
@@ -15,7 +16,19 @@ def choose_random_in_a_list(mylist):
     print(random.choice(mylist))
     print(random.sample(mylist, k=2))
 
+def test_python_debugger():
+    x = [1,2]
+    y = 5
+    z = 6
+    a = y + z 
+    try:
+        pdb.set_trace() # python debugger and can explore different values, use "q" to exit debugger
+        b = x + y # throw an error here
+    except:
+        print("exception occurs")
+
 if __name__ == "__main__":
     generate_random_between_range(1,100)
     choose_random_in_a_list(list(range(10)))
+    test_python_debugger()
 
